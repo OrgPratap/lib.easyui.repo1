ee50f4089aecd2d100df8b2028094db04b68009f
OrgPratap <org.pratap@gmail.com>
2015-09-29 19:21:41 +0530

    initial commit
    "easyui demo added part-01"

---
+++ mvn-app-generator.bat
@@ -0,0 +1,2 @@
+cd C:\Users\singsate\Desktop\freshstart
+mvn archetype:generate -DgroupId=com.pratap.easyui -DartifactId=easyui-parent-pom -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
---
+++ mvn-webapp-generator.bat
@@ -0,0 +1,2 @@
+cd C:\Users\singsate\Desktop\freshstart\easyui-parent-pom
+mvn archetype:generate -DgroupId=com.pratap.easyui -DartifactId=easyui-ex-mergedatagridcells -DarchetypeArtifactId=maven-archetype-webapp
